55319c985208075b1c4c299b4f424c9ef77f23e8
hisham rehab <128677920+hishamrehab@users.noreply.github.com>
2023-08-24 03:49:01 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function subtract() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  return (
+    <div className="counter">
+      <button className="counter--minus" onClick={subtract}>
+        _
+      </button>
+      <div className="counter--count">
+        <h1>{count}</h1>
+      </div>
+      <button className="counter--plus" onClick={add}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default App;
